52733ab0d15697a0b5687eec335d4b00ebbc7af8
vxr <daniel@raygoza.net>
2021-02-01 09:21:11 -0800

    Update DeathKnightUnholy.lua
    dk interrupt fixes

--- Classes/DeathKnightUnholy.lua
+++ Classes/DeathKnightUnholy.lua
@@ -1256,7 +1256,7 @@ if UnitClassBase( "player" ) == "DEATHKNIGHT" then
             id = 47528,
             cast = 0,
             cooldown = 15,
-            gcd = "spell",
+            gcd = "off",
 
             spend = 0,
             spendType = "runic_power",
@@ -1267,7 +1267,9 @@ if UnitClassBase( "player" ) == "DEATHKNIGHT" then
             toggle = "interrupts",
 
             debuff = "casting",
-            readyTime = state.timeToInterrupt,
+            readyTime = function ()
+                return max(state.timeToInterrupt() - 0.25, 0)
+            end,
 
             handler = function ()
                 if conduit.spirit_drain.enabled then gain( conduit.spirit_drain.mod * 0.1, "runic_power" ) end
